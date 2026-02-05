c735c1cd5895501d782c72a17a01e531d42834ee
Mohammad Hussain <mohammad.x.hussain@goindigo.in>
2026-02-05 22:38:19 +0530

    initiating project

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div className=" w-screen h-screen bg-black">
+        <div>
+          <p className="text-white flex-1 text-center">Hello World</p>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
